0fce17d8f16043f8312594a26bfc17128f9c9101
vsh <88970656+u32int@users.noreply.github.com>
2022-08-11 17:17:19 +0200

    feat: add more themes

--- colors.c
+++ colors.c
@@ -1,30 +1,53 @@
 #include "colors.h"
-#include "game.h"
 
-ThemeColors Colors_Default = {
+ThemeData Colors_Default = {
+    .prettyName = "Default",
     .bg      = {18, 18, 18, 255},
     .dim     = {60, 60, 60, 255},
     .error   = {240, 30, 20, 150},
     .primary = {240, 240, 240, 255},
 };
 
-ThemeColors Colors_DefaultLight = {
+ThemeData Colors_DefaultLight = {
+    .prettyName = "Default Light",
     .bg      = {244, 244, 227, 255},
     .dim     = {160, 160, 160, 255},
     .error   = {240, 30, 20, 255},
     .primary = {22, 22, 22, 255},
 };
 
+ThemeData Colors_DarkYellow = {
+    .prettyName = "Dark-Yellow",
+    .bg      = {18, 18, 18, 255},
+    .dim     = {60, 60, 60, 255},
+    .error   = {240, 30, 20, 150},
+    .primary = {240, 220, 35, 255},
+};
+
+ThemeData Colors_SolarizedDark = {
+    .prettyName = "Solarized Dark",
+    .bg      = {0, 43, 54, 255},
+    .dim     = {101, 123, 131, 255},
+    .error   = {220, 50, 47, 150},
+    .primary = {238, 232, 213, 255},
+};
+
 void set_theme(game_t *game, Theme theme)
 {
+    game->selTheme = theme;
+
     switch(theme) {
-    case Default:
-        game->theme = &Colors_Default;
-        break;
     case DefaultLight:
         game->theme = &Colors_DefaultLight;
         break;
-    default: {};
+    case DarkYellow:
+        game->theme = &Colors_DarkYellow;
+        break;
+    case SolarizedDark:
+        game->theme = &Colors_SolarizedDark;
+        break;
+    default:
+        game->theme = &Colors_Default;
     }
 }
 

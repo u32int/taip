8a98ab46b7241856aefa8598f04ef99907b8a797
vsh <88970656+u32int@users.noreply.github.com>
2022-09-28 23:53:49 +0200

    fix: improve util functions

--- util.c
+++ util.c
@@ -12,21 +12,35 @@ int clamp_int(int a, int min, int max)
 
 bool str_starts_with(const char* substr, const char* str)
 {
-    for (size_t i = 0; i < strlen(substr); ++i) {
+    const size_t str_len = strlen(str);
+    const size_t substr_len = strlen(substr);
+
+    if (str_len < substr_len)
+        return false;
+
+    for (size_t i = 0; i < str_len; ++i) {
         if (str[i] != substr[i]) {
             return false;
         }
     }
+
     return true;
 }
 
 bool str_ends_with(const char* substr, const char* str)
 {
-    for (size_t i = 0; i < strlen(substr); ++i) {
-        if (str[strlen(str)-1-i] != substr[strlen(substr)-1-i]) {
+    const size_t str_len = strlen(str);
+    const size_t substr_len = strlen(substr);
+
+    if (str_len < substr_len)
+        return false;
+
+    for (size_t i = 0; i < substr_len; ++i) {
+        if (str[str_len-1-i] != substr[substr_len-1-i]) {
             return false;
         }
     }
+
     return true;
 }
 
